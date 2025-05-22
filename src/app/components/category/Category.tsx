a4df26c0fd0d3f21af02baff1b7b81fe52bc4050
kostikroverist <kostikzhora@gmail.com>
2025-05-22 20:35:26 +0300

    update category

--- src/app/components/category/Category.tsx
+++ src/app/components/category/Category.tsx
@@ -25,6 +25,7 @@ export const CategoryCard: React.FC<CategoryCardProps> = ({
       </div>
 
       <button
+      onClick={onClick}
         type="button"
         className="shadow-md  text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600
          hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800   font-medium rounded-b-lg text-sm px-5 py-2.5 text-center  w-full"
